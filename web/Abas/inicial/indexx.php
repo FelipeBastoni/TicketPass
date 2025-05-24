29fd35c4d90aff6766480dabbc613b78bfbc5398
Felipe Bertão Bastoni <163520381+FelipeBastoni@users.noreply.github.com>
2025-05-24 14:24:23 -0300

    Update indexx.php

--- web/Abas/inicial/indexx.php
+++ web/Abas/inicial/indexx.php
@@ -104,7 +104,7 @@ if(isset($_SESSION['on'])){
         </div>
 
 
-    <p style="color: white">Olá mina do exu</p>
+    <p style="color: white">Olá</p>
 
                             <input type="file" class="form-control" name="inputImagem" id="inputImagem" placeholder="Escolha uma imagem" />
 
