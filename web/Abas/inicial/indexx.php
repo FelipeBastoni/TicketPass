ca6e151697e4d95e17f84f11d168b079a63c3492
Felipe Bertão <felipebertaobastoni@gmail.com>
2025-05-24 14:20:15 -0300

    ALOU

--- web/Abas/inicial/indexx.php
+++ web/Abas/inicial/indexx.php
@@ -104,7 +104,7 @@ if(isset($_SESSION['on'])){
         </div>
 
 
-    <p style="color: white">Olá amigo</p>
+    <p style="color: white">Olá mina do exu</p>
 
                             <input type="file" class="form-control" name="inputImagem" id="inputImagem" placeholder="Escolha uma imagem" />
 
